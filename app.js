19656afe29407930d005bce5b4abcf5d2e7986f9
Aspen Hsu <aspenhsu@Aspens-MBP-2.lan>
2021-10-19 22:11:45 -0400

    Commiting 90% done website landing page
    Included LFS tracking on video

---
+++ app.js
@@ -0,0 +1,50 @@
+const rellax = new Rellax('.rellax');
+
+// grabs the media query
+const motionMediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+
+// if the user prefers reduced motion, disable parallax
+if (motionMediaQuery.matches) rellax.destroy();
+
+// set up text to print, each item in array is new line
+var aText = new Array(
+"awakening spring 2022"
+);
+var iSpeed = 200; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+
+typewriter();
+
+
+
+
+
